0d842e2ca9ac156fb92d5d90790e27fbd43e6f61
martincz <martincz@mokeedev.com>
2014-02-13 23:12:55 +0800

    Apollo: theme style fix
    Change-Id: I6e085e1c6bb546f7c4d2155f7c162b536e05d674

--- src/com/andrew/apollo/ui/fragments/profile/FavoriteFragment.java
+++ src/com/andrew/apollo/ui/fragments/profile/FavoriteFragment.java
@@ -42,6 +42,7 @@ import com.andrew.apollo.provider.FavoritesStore;
 import com.andrew.apollo.recycler.RecycleHolder;
 import com.andrew.apollo.utils.MusicUtils;
 import com.andrew.apollo.utils.NavUtils;
+import com.andrew.apollo.utils.PreferenceUtils;
 import com.andrew.apollo.widgets.ProfileTabCarousel;
 import com.andrew.apollo.widgets.VerticalScrollListener;
 
@@ -313,6 +314,7 @@ public class FavoriteFragment extends Fragment implements LoaderCallbacks<List<S
             // Set the empty text
             final TextView empty = (TextView)mRootView.findViewById(R.id.empty);
             empty.setText(getString(R.string.empty_favorite));
+            empty.setTextColor(PreferenceUtils.getInstance(getActivity()).getDefaultThemeColor(getActivity()));
             mListView.setEmptyView(empty);
             return;
         }
